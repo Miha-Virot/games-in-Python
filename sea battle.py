3deb10dcae27d541c208988dd3e4a4f905b627a6
Михаил Вирот <61559471+Miha-Virot@users.noreply.github.com>
2020-10-23 14:30:41 +0300

    Update sea battle.py

--- sea battle.py
+++ sea battle.py
@@ -39,10 +39,14 @@ def pole():
     for i in range(8):
         print(i+1, end=" ")
         for j in range(8):
-            if NP[j][i] != 0:
+            if NP[j][i] == 1:
                 print("@", end=" ")
             elif NP[j][i] == 2:
                 print("#", end=" ")
+            elif NP[j][i]==3:
+                print("%", end=" ")
+            elif NP[j][i] == 4:
+                print("$", end=" ")                
             else:
                 print("~", end=" ")
         print(sep=" ")
@@ -69,16 +73,14 @@ def shlup():
             p1+=1
             pole()
             return 0
+            
         if n1>1:
             if n1==2:
                 stroka1, stroka2=input("Координаты двупалубной шляндры - ").split()
-                p2+=1
             elif n1==3:
                 stroka1, stroka2=input("Координаты трёхпалубной шляпы - ").split()
-                p3+=1
             elif n1==4:
                 stroka1, stroka2=input("Координаты четырёхпалубной швабры - ").split()
-                p4+=1
 
             x1, y1= stroka1[0], int(stroka1[1])
             x2, y2= stroka2[0], int(stroka2[1])
@@ -88,19 +90,69 @@ def shlup():
                 if chm[0][i]==x2:
                     x2=i+1
             if n1==2:
-                YP[x1-1][y1-1]=2
-                YP[x2-1][y2-1]=2
+                if (x1==x2 or y1==y2) and abs(x1-x2)==1 or abs(y1-y2)==1:
+                    p2+=1
+                    YP[x1-1][y1-1]=2
+                    YP[x2-1][y2-1]=2
             elif n1==3:
-                YP[x1-1][y1-1]=3
-                YP[x2-1][y2-1]=3
+                if (x1==x2 or y1==y2) and abs(x1-x2)==2 or abs(y1-y2)==2:
+                    p3+=1
+                    if x1-x2 == 0:
+                        if y1 > y2:
+                            YP[x1-1][y1-1]=3
+                            YP[x1-1][y1-2]=3
+                            YP[x2-1][y2-1]=3
+                        else:
+                            YP[x1-1][y1-1]=3
+                            YP[x1-1][y2-2]=3
+                            YP[x2-1][y2-1]=3
+                    else:
+                        if x1 > x2:
+                            YP[x1-1][y1-1]=3
+                            YP[x1-2][y1-1]=3
+                            YP[x2-1][y2-1]=3
+                        else:
+                            YP[x1-1][y1-1]=3
+                            YP[x2-2][y2-1]=3
+                            YP[x2-1][y2-1]=3
+                else:
+                    print("Поздравляю, ты долбоёб. Введи нормально")
+                    return 0
             elif n1==4:
-                YP[x1-1][y1-1]=4
-                YP[x2-1][y2-1]=4
+                if (x1==x2 or y1==y2) and abs(x1-x2)==3 or abs(y1-y2)==3:
+                    p4+=1
+                    if x1-x2 == 0:
+                        if y1 > y2:
+                            YP[x1-1][y1-1]=4
+                            YP[x1-1][y1-2]=4
+                            YP[x1-1][y1-3]=4
+                            YP[x2-1][y2-1]=4
+                        else:
+                            YP[x1-1][y1-1]=4
+                            YP[x1-1][y2-2]=4
+                            YP[x1-1][y2-3]=4
+                            YP[x2-1][y2-1]=4
+                    else:
+                        if x1 > x2:
+                            YP[x1-1][y1-1]=4
+                            YP[x1-2][y1-1]=4
+                            YP[x1-3][y1-1]=4
+                            YP[x2-1][y2-1]=4
+                        else:
+                            YP[x1-1][y1-1]=4
+                            YP[x2-2][y2-1]=4
+                            YP[x2-3][y2-1]=4
+                            YP[x2-1][y2-1]=4
+                else:
+                    print("Поздравляю, ты долбоёб. Введи нормально")
+                    return 0
             pole()
             return 0
     else:
-        print('Многовато желаешь милок')
+        print('Многовато желаешь, милок')
+        return 0
 pole()
 
 while shlup()==0:
     n=n
+
