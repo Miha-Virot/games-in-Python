c2b22b4c8b0037ad0c83e4e64464d62427ceed9b
Михаил Вирот <61559471+Miha-Virot@users.noreply.github.com>
2020-10-23 10:32:03 +0300

    Update sea battle.py

--- sea battle.py
+++ sea battle.py
@@ -7,10 +7,13 @@ port = 60666
 
 n=int(8)
 stroka=''
-sdvig=0
+n=8
 
 YP=np.zeros((n,n))
 NP=np.zeros((n,n))
+chm = np.zeros((int(2),n))
+chm=[["A", "B", "C", "D", "E", "F", "G", "H"],[1, 2, 3, 4, 5, 6, 7, 8]]
+
 
 def pole():
     os.system("cls")
@@ -23,6 +26,10 @@ def pole():
                 print("@", end=" ")
             elif YP[j][i] == 2:
                 print("#", end=" ")
+            elif YP[j][i]==3:
+                print("%", end=" ")
+            elif YP[j][i] == 4:
+                print("$", end=" ")                
             else:
                 print("~", end=" ")
         print(sep=" ")
@@ -40,30 +47,60 @@ def pole():
                 print("~", end=" ")
         print(sep=" ")
 
-pole()
-stroka=input("Координаты однопалубной шлюшки - ")
-x, y= stroka[0], int(stroka[1])
-chm = np.zeros((int(2),n))
-chm=[["A", "B", "C", "D", "E", "F", "G", "H"],[1, 2, 3, 4, 5, 6, 7, 8]]
-for i in range(8):
-    if chm[0][i]==x:
-        x=i+1
-YP[x-1][y-1]=1
+p1=0
+p2=0
+p3=0
+p4=0
+e = 0
+
+def shlup():
+    global e, p1, p2, p3, p4
+    n1=int(input("Сколько палуб у шлюхи? "))
+    if p1 == 4 and p2 == 3 and p3 == 2 and p4 == 1: 
+        return 1
+    if n1 == 1 and p1 < 4 or n1 == 2 and p2 < 3 or n1 == 3 and p3 < 2 or n1 == 4 and p4 < 1:
+        if n1==1:
+            stroka=input("Координаты однопалубной шлюшки - ")
+            x, y= stroka[0], int(stroka[1])
+            for i in range(8):
+                if chm[0][i]==x:
+                    x=i+1
+            YP[x-1][y-1]=1
+            p1+=1
+            pole()
+            return 0
+        if n1>1:
+            if n1==2:
+                stroka1, stroka2=input("Координаты двупалубной шляндры - ").split()
+                p2+=1
+            elif n1==3:
+                stroka1, stroka2=input("Координаты трёхпалубной шляпы - ").split()
+                p3+=1
+            elif n1==4:
+                stroka1, stroka2=input("Координаты четырёхпалубной швабры - ").split()
+                p4+=1
+
+            x1, y1= stroka1[0], int(stroka1[1])
+            x2, y2= stroka2[0], int(stroka2[1])
+            for i in range(8):
+                if chm[0][i]==x1:
+                    x1=i+1
+                if chm[0][i]==x2:
+                    x2=i+1
+            if n1==2:
+                YP[x1-1][y1-1]=2
+                YP[x2-1][y2-1]=2
+            elif n1==3:
+                YP[x1-1][y1-1]=3
+                YP[x2-1][y2-1]=3
+            elif n1==4:
+                YP[x1-1][y1-1]=4
+                YP[x2-1][y2-1]=4
+            pole()
+            return 0
+    else:
+        print('Многовато желаешь милок')
 pole()
 
-while sdvig==0:
-    stroka1, stroka2=input("Координаты двупалубной шляндры - ").split()
-    x1, y1= stroka1[0], int(stroka1[1])
-    x2, y2= stroka2[0], int(stroka2[1])
-    chm = np.zeros((int(2),n))
-    chm=[["A", "B", "C", "D", "E", "F", "G", "H"],[1, 2, 3, 4, 5, 6, 7, 8]]
-    for i in range(8):
-        if chm[0][i]==x1:
-            x1=i+1
-        elif chm[0][i]==x2:
-            x2=i+1
-    if (abs(x1-x2)!=1 and y1==y2) or (abs(y1-y2)!=1 and x1==x2):
-        YP[x1-1][y1-1]=2
-        YP[x2-1][y2-1]=2
-        sdvig=1
-        pole()
+while shlup()==0:
+    n=n
